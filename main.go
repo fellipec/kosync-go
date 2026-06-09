584b434c6712c4e0dbb3e045e72f23b9fa8254cf
Luiz Fellipe Carneiro <fellipec@outlook.com>
2026-06-09 17:48:12 -0300

    Added the handle for /healthcheck

---
+++ main.go
@@ -0,0 +1,32 @@
+// kosync-go is a Go implementation of the KOReader Sync Server.
+// It keeps everything as simple as possible, avoiding Redis server, being
+// just a single executable that can run without any config files.
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+Port is the default port the server listens on.
+
+	The original Koreader Sync Server listens on port 7200 by default for
+	TLS connections and on port 17200 for plain HTTP.
+*/
+const Port = 17200
+
+func main() {
+
+	// Handles /healthcheck, which just returns State: OK for troubleshooting purposes.
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, "{\"state\":\"OK\"}")
+	})
+
+	// Creates the HTTP listener
+	addr := fmt.Sprintf(":%d", Port)
+	fmt.Printf("kosync-go listening on port %d\n", Port)
+	http.ListenAndServe(addr, nil)
+
+}
